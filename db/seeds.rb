099abe562d177ed96d11c3f267e184e76e003c6b
RosanaAlcheva <alcheva_rosana@yahoo.com>
2015-01-17 21:11:04 +0100

    Database elements added...

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,20 @@ v8 = Vinarija.create(ime: 'Vinarija Popov', region: 'Tikveski', grad: 'Kavadarci
 v9 = Vinarija.create(ime: 'Popova Kula', region: 'Demir Kapija', grad: 'Demir Kapija', godina: 2005, opis: 'Mnogu ubava vinarija', veb_strana: 'http://www.popovakula.com.mk/', slika: 'http://www.popovakula.com.mk/images/vina/Roze.gif')
 
 #vina
-Vino.create(ime: 'Vranec', vid: 'suvo', tip: 'klasik', sorta: 'Vranec', boja: 'crveno', vinarija_id: v2.id)
-Vino.create(ime: 'Tga za jug', vid: 'polusuvo', tip: 'tradicionalno', sorta: 'Vranec', boja: 'crveno', vinarija_id: v1.id)
+vino1 = Vino.create(ime: 'Vranec', vid: 'suvo', tip: 'klasik', sorta: 'Vranec', boja: 'crveno', vinarija_id: v2.id)
+vino2 = Vino.create(ime: 'Tga za jug', vid: 'polusuvo', tip: 'tradicionalno', sorta: 'Vranec', boja: 'crveno', vinarija_id: v1.id)
+
+#hrana
+hrana_cheese = Hrana.crate(ime: "Sirenje" , slika: "cheese.jpg")
+hrana_chicken = Hrana.crate(ime: "Pilesko meso" , slika: "chicken.jpg")
+hrana_desert = Hrana.crate(ime: "Desert" , slika: "desert.jpg")
+hrana_fish = Hrana.crate(ime: "Riba" , slika: "fish.jpg")
+hrana_fruit = Hrana.crate(ime: "Obosje" , slika: "fruit.jpg")
+hrana_meat = Hrana.crate(ime: "Meso" , slika: "meat.jpg")
+hrana_pasta = Hrana.crate(ime: "Testenini" , slika: "pasta.jpg")
+hrana_seafood = Hrana.crate(ime: "Morska hrana" , slika: "seafood.jpg")
+hrana_vegetable = Hrana.crate(ime: "Zelencuk" , slika: "vegetable.jpg")
+
+#sluzenje
+Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrvena.png", hrana: hrana_cheese.id, vino: vino1)
+Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrvena.png", hrana: hrana_meat.id, vino: vino1)
