e5434db03986aa34471cfeae662f4b3c869e5571
RosanaAlcheva <alcheva_rosana@yahoo.com>
2015-01-27 19:17:49 +0100

    Komentaris model validation and skeleton views

--- app/models/komentari.rb
+++ app/models/komentari.rb
@@ -1,2 +1,10 @@
 class Komentari < ActiveRecord::Base
+  validates_presence_of :ime, :vid, :tip, :sorta, :boja, :slika, :vinarija
+  validates_length_of :ime,
+                      :minimum => 1,           # more than 1 characters
+                      :maximum => 30,          # shorter than 30 characters
+                      :in => 1..30,            # between 1 and 30 characters
+                      :too_short => 'Многу е кратко за име! / Too short for name!',
+                      :too_long => 'Многу е долго за име! / Too long for a name!'
+  validates_uniqueness_of
 end
