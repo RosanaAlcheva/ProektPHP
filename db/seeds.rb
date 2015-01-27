1c4284e11c523357887ccd5b6227099946c87dc4
martin04 <martin04@t-home.mk>
2015-01-27 16:59:04 +0100

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#vinarii
-v1 = Vinarija.create(ime: 'Tikves', region: 'Tikveski', grad: 'Kavadarci', godina: 1885, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://tikves.com.mk', slika: 'tikves.png')
-v2 = Vinarija.create(ime: 'Stobi', region: 'Gradsko', grad: 'Gradsko', godina: 1885, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.stobiwinery.mk', slika: 'stobi.png')
-v3 = Vinarija.create(ime: 'Imako Vino', region: 'Stip', grad: 'Stip', godina: 2002, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://imakovino.com.mk', slika: 'imako2.png')
-v4 = Vinarija.create(ime: 'Valandovo', region: 'Valandovo', grad: 'Valandovo', godina: 1948, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.vizbavalandovo.com.mk', slika: 'valandovo.png')
-v5 = Vinarija.create(ime: 'Skovin', region: 'Skopje', grad: 'Skopje', godina: 1979, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.skovin.mk', slika: 'skovin.png')
-v7 = Vinarija.create(ime: 'Bovin', region: 'Negotino', grad: 'Negotino', godina: 1998, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.bovin.com.mk/', slika: 'bovin.png')
-v8 = Vinarija.create(ime: 'Vinarija Popov', region: 'Tikveski', grad: 'Kavadarci', godina: 2001, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.popovwinery.com.mk/', slika: 'popov.png')
-v9 = Vinarija.create(ime: 'Popova Kula', region: 'Demir Kapija', grad: 'Demir Kapija', godina: 2005, opis: 'Mnogu ubava vinarijas', veb_strana: 'http://www.popovakula.com.mk/', slika: 'popova.png')
+#VINARII
+v1 = Vinarija.create(ime: 'Тиквеш', region: 'Тиквешки', grad: 'Кавадарци', godina: 1885, opis: 'Со повеќе од 125 години работа, оваа винарска визба е најзначајнот творец на моќните македонски вина.', veb_strana: 'http://tikves.com.mk', slika: 'tikves.png')
+v2 = Vinarija.create(ime: 'Стоби', region: 'Градско', grad: 'Градско', godina: 2009, opis: 'Најдоброто грозје го заслужува само најдоброто. Само од комбинацијата на знаење, искуство може да се создаде најдоброто и највкусно вино.', veb_strana: 'http://www.stobiwinery.mk', slika: 'stobi.png')
+v3 = Vinarija.create(ime: 'Имако Вино', region: 'Штипски', grad: 'Штип', godina: 2002, opis: 'Трета најголема винарска визба со цел да понуди квалитетно вино, за секоја маса, како на домашниот така и на глобалниот пазар.', veb_strana: 'http://imakovino.com.mk', slika: 'imako2.png')
+v4 = Vinarija.create(ime: 'Шато Камник', region: 'Скопски', grad: 'Скопје', godina: 2004, opis: 'Автентично македонска, винаријата е израз на величественоста на древниот македонски дух, традиции и почва, и тоа преку еден современ стил на создавање на вино.', veb_strana: 'http://chateaukamnik.com/', slika: 'kamnik.png')
 
-#vina
-vino1 = Vino.create(ime: 'Vranec', vid: 'suvo', tip: 'klasik', sorta: 'Vranec', boja: 'crveno', slika: '/', vinarija_id: v2.id)
-vino2 = Vino.create(ime: 'Tga za jug', vid: 'polusuvo', tip: 'tradicionalno', sorta: 'Vranec', boja: 'crveno', slika: '/', vinarija_id: v1.id)
+#VINA
+vinoTikves1 = Vino.create(ime: 'Alexandria Cuvée', vid: 'Суво', tip: 'Класик', sorta: 'Вранец и мерло', boja: 'Розе', slika: 'alexandriacuvee.png', vinarija_id: v1.id)
+vinoTikves2 = Vino.create(ime: 'Т\'га за југ', vid: 'Полусуво', tip: 'Традиционално', sorta: 'Вранец', boja: 'Црвено', slika: 'tgazajug.png', vinarija_id: v1.id)
+vinoTikves3 = Vino.create(ime: 'Смедеревка', vid: 'Суво', tip: 'Традиционално', sorta: 'Смедеревка', boja: 'Бело', slika: 'smederevka.png', vinarija_id: v1.id)
+#stobi
+vinoStobi1 = Vino.create(ime: 'Шардоне', vid: 'Суво', tip: 'Класик', sorta: 'Шардоне', boja: 'Бело', slika: 'sardone.png', vinarija_id: v2.id)
+vinoStobi2 = Vino.create(ime: 'Вранец', vid: 'Суво', tip: 'Класик', sorta: 'Вранец', boja: 'Црвено', slika: 'vranec.png', vinarija_id: v2.id)
+vinoStobi3 = Vino.create(ime: 'Розе', vid: 'Полусуво', tip: 'Традиционално', sorta: 'Пино Ноар', boja: 'Розе', slika: 'roze.png', vinarija_id: v2.id)
+#imako
+vinoImako1 = Vino.create(ime: 'Пино Ноар', vid: 'Суво', tip: 'Класик', sorta: 'Пино Ноар', boja: 'Црвено', slika: 'pinotnoir.png', vinarija_id: v3.id)
+vinoImako2 = Vino.create(ime: 'Мерлот', vid: 'Суво', tip: 'Традиционално', sorta: 'Мерлот', boja: 'Црвено', slika: 'merlot.png', vinarija_id: v3.id)
+vinoImako3 = Vino.create(ime: 'Ризлинг', vid: 'Суво', tip: 'Традиционално', sorta: 'Ризлинг', boja: 'Бело', slika: 'rizling.png', vinarija_id: v3.id)
+#Kamnik
+vinoKamnik1 = Vino.create(ime: 'Premium Rose Sexy', vid: 'Суво', tip: 'Класик', sorta: 'Sangiovese, Nebiollo, Aglianico и Cabernet Sauvignon', boja: 'Розе', slika: 'premiumroze.png', vinarija_id: v4.id)
+vinoKamnik2 = Vino.create(ime: 'Ten Barrels Syrah', vid: 'Полусуво', tip: 'Традиционално', sorta: 'Шираз', boja: 'Црвено', slika: 'tenbarrels.png', vinarija_id: v4.id)
+vinoKamnik3 = Vino.create(ime: 'Темјаника', vid: 'Суво', tip: 'Традиционално', sorta: 'Темјаника', boja: 'Бело', slika: 'temjanika.png', vinarija_id: v4.id)
 
-#sluzenje
-s1 = Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrveno.png", vino_id: vino1.id)
-s2 = Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrveno.png", vino_id: vino2.id)
 
-#hrana
+#SLUZENJE
+sTikves1 = Sluzenje.create(temperatura_max: "14" ,temperatura_min: "12", slika_chasa: "casaRoze.png", vino_id: vinoTikves1.id)
+sTikves2 = Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrveno.png", vino_id: vinoTikves2.id)
+sTikves3 = Sluzenje.create(temperatura_max: "12" ,temperatura_min: "10", slika_chasa: "casaBelo.png", vino_id: vinoTikves3.id)
+#stobi
+sStobi1 = Sluzenje.create(temperatura_max: "12" ,temperatura_min: "10", slika_chasa: "casaBelo.png", vino_id: vinoStobi1.id)
+sStobi2 = Sluzenje.create(temperatura_max: "17" ,temperatura_min: "15", slika_chasa: "casaCrveno.png", vino_id: vinoStobi2.id)
+sStobi3 = Sluzenje.create(temperatura_max: "8" ,temperatura_min: "6", slika_chasa: "casaRoze.png", vino_id: vinoStobi3.id)
+#imako
+sImako1 = Sluzenje.create(temperatura_max: "14" ,temperatura_min: "12", slika_chasa: "casaCrveno.png", vino_id: vinoImako1.id)
+sImako2 = Sluzenje.create(temperatura_max: "18" ,temperatura_min: "16", slika_chasa: "casaCrveno.png", vino_id: vinoImako2.id)
+sImako3 = Sluzenje.create(temperatura_max: "12" ,temperatura_min: "10", slika_chasa: "casaBelo.png", vino_id: vinoImako3.id)
+#Kamnik
+sKamnik1 = Sluzenje.create(temperatura_max: "10" ,temperatura_min: "8", slika_chasa: "casaRoze.png", vino_id: vinoKamnik1.id)
+sKamnik2 = Sluzenje.create(temperatura_max: "17" ,temperatura_min: "13", slika_chasa: "casaCrveno.png", vino_id: vinoKamnik2.id)
+sKamnik3 = Sluzenje.create(temperatura_max: "10" ,temperatura_min: "8", slika_chasa: "casaBelo.png", vino_id: vinoKamnik3.id)
+
+
+#HRANA
 hrana_cheese = Hrana.create(ime: "Sirenje" , slika: "cheese.jpg")
 hrana_chicken = Hrana.create(ime: "Pilesko meso" , slika: "chicken.jpg")
 hrana_desert = Hrana.create(ime: "Desert" , slika: "desert.jpg")
@@ -36,7 +60,38 @@ hrana_seafood = Hrana.create(ime: "Morska hrana" , slika: "seafood.jpg")
 hrana_vegetable = Hrana.create(ime: "Zelencuk" , slika: "vegetable.jpg")
 
 #join table Sluzenje_Hrana
-SluzenjeHrana.create({sluzenje_id: s1.id, hrana_id: hrana_cheese.id})
-SluzenjeHrana.create({sluzenje_id: s1.id, hrana_id: hrana_meat.id})
-SluzenjeHrana.create({sluzenje_id: s2.id, hrana_id: hrana_cheese.id})
-SluzenjeHrana.create({sluzenje_id: s2.id, hrana_id: hrana_meat.id})
+#tikves
+SluzenjeHrana.create({sluzenje_id: sTikves1.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sTikves1.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sTikves1.id, hrana_id: hrana_pasta.id})
+SluzenjeHrana.create({sluzenje_id: sTikves1.id, hrana_id: hrana_vegetable.id})
+SluzenjeHrana.create({sluzenje_id: sTikves2.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sTikves2.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sTikves3.id, hrana_id: hrana_chicken.id})
+SluzenjeHrana.create({sluzenje_id: sTikves3.id, hrana_id: hrana_fish.id})
+SluzenjeHrana.create({sluzenje_id: sTikves3.id, hrana_id: hrana_vegetable.id})
+#stobi
+SluzenjeHrana.create({sluzenje_id: sStobi1.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sStobi1.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sStobi1.id, hrana_id: hrana_seafood.id})
+SluzenjeHrana.create({sluzenje_id: sStobi1.id, hrana_id: hrana_vegetable.id})
+SluzenjeHrana.create({sluzenje_id: sStobi1.id, hrana_id: hrana_fish.id})
+SluzenjeHrana.create({sluzenje_id: sStobi2.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sStobi2.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sStobi2.id, hrana_id: hrana_desert.id})
+SluzenjeHrana.create({sluzenje_id: sStobi3.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sStobi3.id, hrana_id: hrana_fish.id})
+SluzenjeHrana.create({sluzenje_id: sStobi3.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sStobi3.id, hrana_id: hrana_fruit.id})
+#imako
+SluzenjeHrana.create({sluzenje_id: sImako1.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sImako2.id, hrana_id: hrana_cheese.id})
+SluzenjeHrana.create({sluzenje_id: sImako2.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sImako3.id, hrana_id: hrana_fish.id})
+SluzenjeHrana.create({sluzenje_id: sImako3.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sImako3.id, hrana_id: hrana_seafood.id})
+#Kamnik
+SluzenjeHrana.create({sluzenje_id: sKamnik1.id, hrana_id: hrana_fruit.id})
+SluzenjeHrana.create({sluzenje_id: sKamnik2.id, hrana_id: hrana_meat.id})
+SluzenjeHrana.create({sluzenje_id: sKamnik3.id, hrana_id: hrana_desert.id})
+SluzenjeHrana.create({sluzenje_id: sKamnik3.id, hrana_id: hrana_fruit.id})
